90c25a079e666a896a159bd4a3afaa38e9e85a6f
matt_wellman <williamwellman98@gmail.com>
2022-12-25 11:43:00 -0500

    chore(config-service):add config test

--- packages/config-service/__test__/index.test.ts
+++ packages/config-service/__test__/index.test.ts
@@ -1,9 +1,29 @@
 'use strict';
 import { ConfigService } from '../src/index';
 import { describe, expect, test } from '@jest/globals';
-describe('return string', () => {
-  test('string returned', () => {
+import { env } from 'process';
+import * as config from 'config';
+
+describe('Test ConfigModule', () => {
+  const hold = ConfigService.prototype.isDev;
+  console.log('\x1b[36m', env, config, '\x1b[0m');
+  test('Pass in Dev Param should return true: ', () => {
     expect(ConfigService).toBeDefined();
+    // mock the config service
+    jest.mock('../src/index', () => {
+      return {
+        ConfigService: jest.fn().mockImplementation(() => {
+          return {
+            get: (property: string): string => {
+              return 'development';
+            },
+            isDev: (): boolean => {
+              return true;
+            },
+          };
+        }),
+      };
+    });
     console.log(`test passed for config-service`);
   });
 });
