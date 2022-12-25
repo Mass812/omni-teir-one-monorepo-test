bafeb8e0ae9263c65aca190f285e20ecb5634a19
matt_wellman <williamwellman98@gmail.com>
2022-12-25 14:01:05 -0500

    spike: verdaccio

--- verdaccio/docker/verdaccio.sh
+++ verdaccio/docker/verdaccio.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+
+
 echo "Starting verdaccio in docker container..."
 
 V_PATH=`pwd`/verdaccio; docker run -it --rm --name verdaccio \
@@ -9,6 +11,7 @@ V_PATH=`pwd`/verdaccio; docker run -it --rm --name verdaccio \
   -v $V_PATH/plugins:/verdaccio/plugins \
   verdaccio/verdaccio
 
-echo "Verdaccio is running in docker container:"
-echo " @ http://localhost:4873"
+
+echo "Verdaccio is running in docker container:  @ http://localhost:4873"
+
 echo " @ http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' verdaccio):4873"
