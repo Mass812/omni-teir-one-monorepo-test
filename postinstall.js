e2fddb9e997e305fcabeb5d8b496fe7478006a5c
matt_wellman <williamwellman98@gmail.com>
2023-01-01 02:17:38 -0500

    refactor(install): stdout for logging git workflow values for error breadcrumbs

--- postinstall.js
+++ postinstall.js
@@ -10,6 +10,7 @@ import { spawnStreaming } from '@lerna/child-process'
 import process from 'node:process';
 import fs from 'node:fs';
 import pjson from './package.json' assert { type: 'json' };
+import { stdout } from 'process';
 // get project root
 const root = process.cwd();
 
@@ -18,6 +19,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __folderName = path.dirname(__filename);
 console.warn('__filename: ', __filename)
 console.warn('__folderName: ', __folderName);
+
+// remove root from path
+const absolutePath = __filename.split('packages/')[0]
+const formPath = __filename.slice(0, absolutePath.length)
+
+console.log('\x1b[36m', {absolutePath}, {formPath}, absolutePath.length, {root} ,'\x1b[0m');
 
 const packages = getPackagesSync()
 
@@ -31,7 +38,18 @@ packages
       const packageWithPatch = pkg.location.split('/').reverse()[0]
 
       console.log('\x1b[36m', 'patches found in ==> : ',`${packageWithPatch} @ ${relPath}`,'\x1b[0m');
-
+      stdout._write('\x1b[36m', 'patches found in ==> : ',`${packageWithPatch} @ ${relPath}`, {
+        __filename,
+        __folderName,
+        absolutePath,
+        formPath,
+        root,
+        pkg,
+        patches,
+        nmPatchModuleIndex,
+        relPath,
+        packageWithPatch
+      }, '\x1b[0m');
     spawnStreaming('node', [nmPatchModuleIndex, '--patch-dir', `${relPath}`], { shell: true })
     }
   })
